3221f98e90a9567776fddd55d82f533e68e5b2e5
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-04-28 15:51:45 +0530

    feat: initialize key value

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: ApiProvider(api: Api(), child: const HomePage()),
     );
+  }
+}
+
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+  ApiProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  })  : uuid = const Uuid().v4(),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    // ignore: unrelated_type_equality_checks
+    return uuid != oldWidget;
+  }
+
+  static ApiProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
   }
 }
 
@@ -25,6 +47,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // ValueKey _textKey = const ValueKey<String>(!null);
   String title = 'Tap the screen';
   @override
   Widget build(BuildContext context) {
@@ -38,10 +61,31 @@ class _HomePageState extends State<HomePage> {
             title = DateTime.now().toIso8601String();
           });
         },
-        child: Container(
-          color: Color.fromARGB(255, 150, 106, 106),
-        ),
+        child: Container(color: Colors.white),
       ),
     );
+  }
+}
+
+class DteTimeWidget extends StatelessWidget {
+  const DteTimeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final api = ApiProvider.of(context).api;
+    return Text(api.dateAndTime ?? 'Tap on  Screen to fetch date and time');
+  }
+}
+
+class Api {
+  String? dateAndTime;
+
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+            const Duration(seconds: 1), () => DateTime.now().toIso8601String())
+        .then((value) {
+      dateAndTime = value;
+      return value;
+    });
   }
 }
